166af90dfa2932d434028fa6c2c2e1ef08998755
Jeremy-Belec <144268291+Jeremy-Belec@users.noreply.github.com>
2026-06-14 15:48:38 -0400

    Update date_night.py

--- date_night.py
+++ date_night.py
@@ -472,4 +472,4 @@ if st.session_state.suboption:
         </div>
         """, unsafe_allow_html=True)
 
-st.markdown('<div class="footer">💕 Vyrobeno s vášní, jen pro vás! PS: ma mère nous a cuisiné une surprise 💕 </div>', unsafe_allow_html=True)
+st.markdown('<div class="footer">💕 Navrženo s vášní a lehkostí, jen pro vás!! PS: ma mère nous a cuisiné une surprise 💕 </div>', unsafe_allow_html=True)
